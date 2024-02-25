5dd553c111479ab6ebdcae02b62f0f0c21b2527f
reira-endo <103876343+reira-endo@users.noreply.github.com>
2024-02-25 06:20:33 +0000

    プロトタイプの見た目改善

--- myapp/public/javascripts/changeMode.js
+++ myapp/public/javascripts/changeMode.js
@@ -5,21 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
     var memoMode = document.getElementById('memoMode');
     var indexMode = document.getElementById('indexMode');
     var dictionaryMode = document.getElementById('dictionaryMode');
+    var memoModeHon = document.getElementById('memoModeHon');
+    var indexModeHon = document.getElementById('indexModeHon');
   
     showMemoBtn.addEventListener('click', function() {
       memoMode.style.display = 'block';
+      memoModeHon.style.display ='block';
       indexMode.style.display = 'none';
+      indexModeHon.style.display ='none';
       dictionaryMode.style.display = 'none';
     });
   
     showIndexBtn.addEventListener('click', function() {
       memoMode.style.display = 'none';
+      memoModeHon.style.display ='none';
       indexMode.style.display = 'block';
+      indexModeHon.style.display ='block';
       dictionaryMode.style.display = 'none';
     });
 
     showDictionaryBtn.addEventListener('click', function() {
     memoMode.style.display = 'none';
+    memoModeHon.style.display ='none';
+    indexModeHon.style.display ='block';
     indexMode.style.display = 'none';
     dictionaryMode.style.display = 'block';
     });
